86c25e4445c16ea50f6e013493d0882fae9c5f1d
SonnxDela <142680493+SonnxDev@users.noreply.github.com>
2026-05-19 00:05:11 -0500

    Rename app component to App in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
 // src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import app from './app.jsx'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <app />
+    <App />
   </React.StrictMode>,
 )
