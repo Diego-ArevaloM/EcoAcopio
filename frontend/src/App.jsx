05ac63b932c1ffd3cbf64e5ce162375811247564
SonnxDela <142680493+SonnxDev@users.noreply.github.com>
2026-05-19 00:05:29 -0500

    Rename app.jsx to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ import ReportesPage from "./pages/reportes";
 import MaterialesPage from "./pages/materiales";
 import AuditoriaPage from "./pages/auditoria";
 
-export default function app() {
+export default function App() {
   const [user, setUser] = useState(null);
   const [role, setRole] = useState(null);
   const [page, setPage] = useState("dashboard");
